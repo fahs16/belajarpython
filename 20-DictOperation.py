d23d42ad24604c424bd434d9f300bf72bc6bf078
Fitrah Ali Hudzaifah Sofyan <fitrah6@gmail.com>
2018-08-14 13:46:10 +0700

    Belajar Python

---
+++ 20-DictOperation.py
@@ -0,0 +1,41 @@
+m = {"buah" : ("jambu","mangga","anggur") }
+n = {"hewan" :"anjing", "serangga" : "nyamuk" , "ikan":"paus"}
+a = "hasil : "
+print("""Dict Operation
+
+	Untuk lebih jelasnya tentang dict operation, 
+	lihat contoh berikut ini :""")
+print("""
+print(m)
+""")
+print(a,m)
+
+print("""
+print(n)
+""")
+print(a,n)
+print("""
+print(len(m))""")
+print(a,len(m))
+print(""""
+print(len(n))
+""")
+print(a,len(n))
+print("""
+o = n.copy()
+""")
+o = n.copy()
+print("""
+print(o)
+""")
+print(a,o)
+print("""
+kita bisa mengubah value dalam key suatu elemen dict
+dengan cara sebagai berikut
+n["hewan"] = "badak
+""")
+n["hewan"] = "badak"
+print("""
+print(n)
+""")
+print(a,n)
