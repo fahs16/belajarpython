7a8508c84f5434ee73f0185ad2bb5b5199a4cba3
Fitrah Ali Hudzaifah Sofyan <fitrah6@gmail.com>
2018-08-14 16:18:04 +0700

    Belajar Python

---
+++ 23-TambahanString.py
@@ -0,0 +1,32 @@
+print("""Tambahan ya...
+
+
+v = """
+orang indo ketawanya wkwkwk
+orang thai ketawanya 555555
+"""
+w = "___wkwkwkwk___"
+
+print(w.strip("_"))
+print(w.replace("w","h").replace("k","a"))
+print(w.rstrip("_"))
+print(w.lstrip("_"))
+
+print(v.split(" "))
+print(v.splitlines())
+
+""")
+
+v = """
+orang indo ketawanya wkwkwk
+orang thai ketawanya 555555
+"""
+w = "___wkwkwkwk___"
+
+print(w.strip("_"))
+print(w.replace("w","h").replace("k","a"))
+print(w.rstrip("_"))
+print(w.lstrip("_"))
+
+print(v.split(" "))
+print(v.splitlines())
